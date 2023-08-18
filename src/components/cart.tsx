eac44de353fd610f7ee7ad88934a0251f4549d0c
onyekachii <emegwaonyeka@gmail.com>
2023-08-18 23:32:23 +0100

    making changes

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -8,17 +8,18 @@ import {
   IonModal,
   IonButtons,
 } from "@ionic/react";
-import React, {  useRef } from "react";
+import React, { useRef } from "react";
 import { useAppSelector } from "../app/hooks";
 import { RootState } from "../app/store";
 import UserDetails from "../pages/userDetails";
 import Cartdetails2 from "./cartdetails";
 
-
 const Cart: React.FC = () => {
   const userDetails = useAppSelector(
     (state: RootState) => state.persistedReducer.userDetails
   );
+
+  console.log(userDetails);
   const modal = useRef<HTMLIonModalElement>(null);
 
   const confirm = () => {
@@ -31,7 +32,7 @@ const Cart: React.FC = () => {
     );
   };
 
-  const openModal = ()  => {
+  const openModal = () => {
     if (modal.current) {
       modal.current.present();
     }
@@ -51,32 +52,39 @@ const Cart: React.FC = () => {
         </div>
 
         {/*****  dissplay modal for user entry **************/}
-        <IonModal ref={modal}>
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot="start">
-                <IonButton onClick={() => modal.current?.dismiss()}>
-                  Cancel
-                </IonButton>
-              </IonButtons>
-              <IonTitle>Welcome</IonTitle>
-              <IonButtons slot="end">
-                <IonButton
-                  strong={true}
-                  routerLink="/checkout"
-                  onClick={() => {
-                    confirm();
-                  }}
-                >
-                  Confirm
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
+        <div> 
+          { userDetails.username === '' ?
+          <IonModal ref={modal}>
+            <IonHeader>
+              <IonToolbar>
+                <IonButtons slot="start">
+                  <IonButton
+                    routerLink="/cart"
+                    onClick={() => modal.current?.dismiss()}
+                  >
+                    Cancel
+                  </IonButton>
+                </IonButtons>
+                <IonTitle>Welcome</IonTitle>
+                <IonButtons slot="end">
+                  <IonButton
+                    strong={true}
+                    routerLink="/checkout"
+                    onClick={() => {
+                      userDetails.username === "" ? null : confirm();
+                    }}
+                  >
+                    Confirm
+                  </IonButton>
+                </IonButtons>
+              </IonToolbar>
+            </IonHeader>
 
-          {/*****  where users input details **************/}
-          <UserDetails />
-        </IonModal>
+            {/*****  where users input details **************/}
+            <UserDetails />
+          </IonModal>
+          : null }
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/components/productPageHeader.tsx
+++ src/components/productPageHeader.tsx
@@ -1,46 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonNavLink,IonButton, IonIcon } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonNavLink,
+  IonButton,
+  IonIcon,
+} from "@ionic/react";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import {cartOutline} from "ionicons/icons";
-import CartPage from '../pages/checkoutPage';
+import { cartOutline } from "ionicons/icons";
+import CartPage from "../pages/checkoutPage";
 import { menuController } from "@ionic/core/components";
-import CartDetails from './cart';
+import CartDetails from "./cart";
 
-
-import React from 'react';
+import React from "react";
 
 const ProductPageHeader: React.FC = () => {
-  const cartValue = useAppSelector((state) => state.persistedReducer.cartValue.cartValue);
+  const cartValue = useAppSelector(
+    (state) => state.persistedReducer.cartValue.cartValue
+  );
 
-async function openEndMenu() {
-  // Open the menu by side
-  await menuController.open("end");
-}
-    return (
-      <div className=" flex items-center justify-between px-4 py-2 border border-primaryColor  mb-5 text-primaryColor text-lg rounded">
-        <p>Product</p>
-        <div className=" relative ">
-          {cartValue === 0 ? null : (
-            <p className="absolute z-10 right-0 top-0 bg-primaryColor text-lightColor rounded-full text-xs w-4 h-4 flex justify-center items-center ">
-              {cartValue}
-            </p>
-          )}
-          <IonButton
-            slot="end"
-            fill='clear'
-            // routerLink='/newpage'
-            size='small'
-            onClick={openEndMenu}
-          >
-            <IonIcon
-              icon={cartOutline}
-              color=""
-              size="large"
-              slot="end"
-            />
-          </IonButton>
-        </div>
+  async function openEndMenu() {
+    // Open the menu by side
+    await menuController.open("end");
+  }
+  return (
+    <div className=" flex items-center justify-between px-4 py-2 border border-primaryColor  mb-5 text-primaryColor text-lg rounded">
+      <p>Product</p>
+      <div className=" relative ">
+        {cartValue === 0 ? null : (
+          <p className="absolute z-10 right-0 top-0 bg-primaryColor text-lightColor rounded-full text-xs w-4 h-4 flex justify-center items-center ">
+            {cartValue}
+          </p>
+        )}
+
+        <IonIcon icon={cartOutline} color="" size="large" slot="end" />
       </div>
-    );
+    </div>
+  );
 };
 
 export default ProductPageHeader;
